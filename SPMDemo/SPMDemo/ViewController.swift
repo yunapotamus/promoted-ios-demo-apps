6c5fc01814dfeccd97f504b36c6454a913316b26
Yuna Wang <28821219+yunapotamus@users.noreply.github.com>
2024-06-18 21:30:49 -0400

    Add project

---
+++ SPMDemo/SPMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMDemo
+//
+//  Created by Yuna Wang on 2024-06-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
